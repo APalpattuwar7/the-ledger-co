b5790eac43cdc2c228c8a3c7575cb551c5325ded
Aditya Palpattuwar <adityapalpattuwar7@gmail.com>
2021-11-24 17:42:33 +0530

    Added logic to read, parse and process input file

---
+++ DataProcessor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TheLedgerCo.Models;
+
+namespace TheLedgerCo
+{
+    public class DataProcessor
+    {
+        public readonly List<Loan> loans;
+
+        public DataProcessor()
+        {
+            loans = new List<Loan>();
+        }
+
+        public void ProcessLoanForBorrower(Data data)
+        {
+            var loan = new Loan()
+            {
+                Bank = new Bank { Name = data.BankName },
+                Borrower = new Borrower { Name = data.BorrowersName },
+                PrincipalAmount = data.Amount,
+                NumberOfYears = data.NumberOfYears,
+                RateOfInterest = data.RateOfInterest
+            };
+
+            loans.Add(loan);
+        }
+
+        public void ProcessPayment(Data data)
+        {
+            Loan loan = GetProcessedLoan(data);
+            loan.RepaymentAmount += data.LumpSumAmount;
+            loan.EMICompletedBeforeRepayment = data.EMINumber;
+        }
+
+        public void ProcessBalance(Data data)
+        {
+            Loan loan = GetProcessedLoan(data);
+            if (loan == null) return;
+
+            var totalAmountToRepay = loan.CalculateTotalAmountToRepay();
+            var emi = loan.CalculateEMI(totalAmountToRepay);
+            var totalAmountPaid = CalculateTotalAmountPaid(data, loan, emi);
+            var remainingEMI = CalculateRemainingEMI(data, loan, totalAmountToRepay, totalAmountPaid, emi);
+
+            Console.WriteLine($"{loan.Bank.Name} {loan.Borrower.Name} {totalAmountPaid} {remainingEMI}");
+        }
+
+        #region PRIVATE Methods
+
+        private static int CalculateRemainingEMI(Data data, Loan loan, decimal totalAmountToRepay, decimal totalAmountPaid, int emi)
+        {
+            int remainingEMI;
+            //If PAYMENT was not done, then remainingEMI calculation is simple and can be calculated using NumberOfYears the loan was taken
+            // otherwise, remainingEMI can be calculated using remaining amount divided by emi.
+            if (loan.RepaymentAmount == 0)
+            {
+                remainingEMI = (loan.NumberOfYears * 12) - data.EMINumber;
+            }
+            else
+            {
+                remainingEMI = (int)Math.Ceiling((totalAmountToRepay - totalAmountPaid) / emi);
+            }
+
+            return remainingEMI;
+        }
+
+        private static decimal CalculateTotalAmountPaid(Data data, Loan loan, int emi)
+        {
+            decimal totalAmountPaid;
+            if (data.EMINumber < loan.EMICompletedBeforeRepayment)
+            {
+                totalAmountPaid = emi * data.EMINumber;
+            }
+            else
+            {
+                totalAmountPaid = emi * data.EMINumber + loan.RepaymentAmount;
+            }
+
+            return totalAmountPaid;
+        }
+
+        private Loan GetProcessedLoan(Data data)
+        {
+            //Find already processed loan based on bank name and borrower's name
+            return loans.Where(loan => loan.Bank.Name == data.BankName && loan.Borrower.Name == data.BorrowersName).SingleOrDefault();
+        } 
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using TheLedgerCo.Models;
+using static TheLedgerCo.Enums.Enums;
+
+namespace TheLedgerCo
+{
+    class Program
+    {
+        private static DataProcessor processor;
+
+        static void Main(string[] args)
+        {
+            ProcessInputFile(args[0]);
+        }
+
+        private static void ProcessInputFile(string filePath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            processor = new DataProcessor();
+            foreach (string line in lines)
+            {
+                ProcessInputData(line);
+            }
+        }
+
+        private static void ProcessInputData(string line)
+        {
+            string[] input = line.Split(' ');
+            string command = input[0];
+            
+            if(command == Command.LOAN.ToString())
+            {
+                var data = new Data()
+                {
+                    BankName = input[1],
+                    BorrowersName = input[2],
+                    Amount = Convert.ToDecimal(input[3]),
+                    NumberOfYears = Convert.ToInt32(input[4]),
+                    RateOfInterest = Convert.ToInt32(input[5])
+                };
+
+                processor.ProcessLoanForBorrower(data);
+
+            } else if(command == Command.BALANCE.ToString())
+            {
+                var data = new Data()
+                {
+                    BankName = input[1],
+                    BorrowersName = input[2],
+                    EMINumber = Convert.ToInt32(input[3])
+                };
+
+                processor.ProcessBalance(data);
+
+            } else if(command == Command.PAYMENT.ToString())
+            {
+                var data = new Data()
+                {
+                    BankName = input[1],
+                    BorrowersName = input[2],
+                    LumpSumAmount = Convert.ToInt32(input[3]),
+                    EMINumber = Convert.ToInt32(input[4])
+                };
+
+                processor.ProcessPayment(data);
+            }
+        }
+    }
+}
